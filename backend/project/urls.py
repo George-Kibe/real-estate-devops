614515003a06f990af4ba85ec9a7eaec95d2bc48
George-Kibe <georgekibewambui@gmail.com>
2024-10-01 22:27:07 +0300

    server status check

--- backend/project/urls.py
+++ backend/project/urls.py
@@ -1,11 +1,17 @@
+"""
+Project URLS Layout
+"""
 from django.views.static import serve
 from django.urls import re_path
 from django.contrib import admin
 from django.urls import path, include
+from .views import ServerStatusView
 
 urlpatterns = [
-    path("admin/", admin.site.urls),
+    path("supersecret/", admin.site.urls),
     path("api/", include('core.urls')),
     path("api/", include('resume.urls')),
-    re_path(r'^.well-known/acme-challenge/(.*)$', serve, {'document_root': r'C:\Users\Administrator\Downloads\project\backend\.well-known\acme-challenge'}),
+    path('', ServerStatusView.as_view(), name='server-status'),
+    re_path(r'^.well-known/acme-challenge/(.*)$', serve,
+            {'document_root': r'C:\Users\Administrator\Downloads\project\backend\.well-known\acme-challenge'}),
 ]
